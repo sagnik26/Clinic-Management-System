aae3eca82c9d6c9655d1502964a760ae55d09618
sagnik26 <sagnikjohnson26@gmail.com>
2022-12-11 02:48:59 +0530

    new update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(logger)
 
 app.options('*', cors(corsOptions));
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://clinictest.onrender.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
