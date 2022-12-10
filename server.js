4fc2643ea147127c61a0b116e8ba45269179115d
sagnik26 <sagnikjohnson26@gmail.com>
2022-12-11 02:43:09 +0530

    new update

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.use(logger)
 // app.use(cors())
 
 app.options('*', cors(corsOptions));
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// })
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use(express.json())
 
