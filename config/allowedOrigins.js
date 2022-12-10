6f8008c8877371ddef56ef8ba21f970be0146504
sagnik26 <sagnikjohnson26@gmail.com>
2022-12-11 02:59:47 +0530

    new update added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://medicalclinic.onrender.com/',
     'https://clinictest.onrender.com/'
 ]
 
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,10 +3,10 @@ const allowedOrigins = require('./allowedOrigins')
 const corsOptions = {
     origin: (origin, callback) => {
         if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            return callback(null, true)
+             callback(null, true)
         }
         else {
-            return callback(new Error('Not allowed by cors'), false)
+             callback(new Error('Not allowed by cors'), false)
         }
     },
     credentials: true,
